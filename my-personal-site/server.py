6ba46e88f92f8ae8c7ba318c639fc5ea44e5d3f6
J-Bills <billsjamal@gmail.com>
2024-06-07 10:48:37 -0700

    created a flask app using previously made html, css, and images

--- my-personal-site/server.py
+++ my-personal-site/server.py
@@ -3,14 +3,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def home():
     #return '<h1>Hello World<h1/>'
     
     #returning the html template that we created
     return render_template('index.html')
 
 @app.route('/angela')
-def angela():    
+def angela():
+        
     #returning the html template that we created
     return render_template('angela.html')
 
