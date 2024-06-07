e47306dddfc46ad1395511d56ec58c0dc7ab4caa
J-Bills <billsjamal@gmail.com>
2024-06-07 09:52:58 -0700

    add angelas html as a template

--- my-personal-site/server.py
+++ my-personal-site/server.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello World<h1/>'
+    #return '<h1>Hello World<h1/>'
+    
+    #returning the html template that we created
+    return render_template('index.html')
+
+@app.route('/angela')
+def angela():    
+    #returning the html template that we created
+    return render_template('angela.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
